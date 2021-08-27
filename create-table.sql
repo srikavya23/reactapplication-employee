97d10e9907f6cf43d0fc7cbf133e6c5092885c60
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-08-27 12:00:43 +0530

    Create create-table.sql

---
+++ create-table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE employee (id serial NOT NULL,name character varying(200) NOT NULL,job character varying(200) NOT NULL,department character varying(20) NOT NULL,salary int NOT NULL,hire_date date NOT NULL);
