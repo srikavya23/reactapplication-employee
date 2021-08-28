4fd53e4c9ce973c79f1ac07246f35476467ee812
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-08-28 17:07:57 +0530

    Update create-table.sql

--- create-table.sql
+++ create-table.sql
@@ -1,4 +1,5 @@
 ALTER USER postgres WITH PASSWORD '1234';
-
-
+DROP DATABASE IF EXISTS testdb;
+CREATE DATABASE testdb;
+\c testdb;
 CREATE TABLE employee (id serial NOT NULL,name character varying(200) NOT NULL,job character varying(200) NOT NULL,department character varying(20) NOT NULL,salary int NOT NULL,hire_date date NOT NULL);
